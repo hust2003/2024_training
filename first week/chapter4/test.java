e46c54305c2009791ff48f65a5f05484ab5f9abf
hust2003 <659073610@qq.com>
2024-06-04 21:32:18 +0800

    Add files via upload

---
+++ first week/chapter4/test.java
@@ -0,0 +1,17 @@
+public class test{
+      public static void main(String[] args) {
+      char aChar='a';
+      char enterChar='\r';
+      char ch='\u9999';
+      System.out.println(ch);
+      char zhong='疯';
+      int zhongValue=zhong;
+      System.out.println(zhongValue);
+      char c=97;
+      System.out.println(c);
+
+
+
+
+      }
+} 
---
+++ first week/chapter4/zifu.java
@@ -0,0 +1,10 @@
+public class zifu{
+     public static void main(String[] args) {
+     	for(var count=10;count<10;count++)
+          {
+               System.out.println(count);
+          }
+          System.out.println("循环结束！");
+     }
+
+}
