059e622ee770288640b5b903dd9cc09b5c2a6b84
hust2003 <659073610@qq.com>
2024-06-12 16:08:18 +0800

    Add files via upload

---
+++ second week/chapter 6/MethodDemo2.java
@@ -0,0 +1,13 @@
+public class MethodDemo2{
+	public static void main(String[] args) {
+		
+           //调用方法
+			printGFInfo();
+		}
+
+		public static void printGFInfo(){
+			System.out.println("1");
+			System.out.println("12");
+			System.out.println("123");
+		}
+}
