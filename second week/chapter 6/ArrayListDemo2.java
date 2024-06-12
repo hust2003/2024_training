bda4a87088e5d2e09007ad2e84df999a91f8b0c9
hust2003 <659073610@qq.com>
2024-06-12 16:06:47 +0800

    Add files via upload

---
+++ second week/chapter 6/ArrayListDemo2.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+public class ArrayListDemo2{
+	public static void main(String[] args) {
+		//创建一个集合
+		ArrayList<String> list=new ArrayList<>();
+		//添加元素
+		list.add("aaa");
+		list.add("bbb");
+		System.out.println(list);
+	}
+}
